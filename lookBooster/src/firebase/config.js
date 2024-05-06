ba3f6c8c6dabf01071c2d99134df5fea308fac02
raayg26 <raayg26@gmail.com>
2024-05-06 17:47:13 +0200

    react start

---
+++ lookBooster/src/firebase/config.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDGhqLEeMrrni_eufSptdgDVUD-llCGr0U",
+  authDomain: "proyectobarberia-981f5.firebaseapp.com",
+  projectId: "proyectobarberia-981f5",
+  storageBucket: "proyectobarberia-981f5.appspot.com",
+  messagingSenderId: "399505533102",
+  appId: "1:399505533102:web:eab1e3731840e0bc9ca53f",
+  measurementId: "G-M1D37Y8WLT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFireStore(app);
