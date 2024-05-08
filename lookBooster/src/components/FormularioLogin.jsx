9511c500f1c1fa5bd42aee0159dbb80bccbea629
raayg26 <raayg26@gmail.com>
2024-05-08 12:36:31 +0200

    login encaminado

--- lookBooster/src/components/FormularioLogin.jsx
+++ lookBooster/src/components/FormularioLogin.jsx
@@ -1,16 +1,47 @@
-
+import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import './FormLogin.css'; 
 import Logo from '../assets/logoCompleto.png';
 import { Link } from 'react-router-dom';
+import { FaGoogle, FaApple } from "react-icons/fa";
 
-
+function RegistroForm() {
+  return (
+    <Form className="signin-form">
+      
+      <div className="form-group mb-4">
+        <Form.Control type="text" placeholder="Nombre de usuario" required />
+      </div>
+      <div className="form-group mb-4">
+        <Form.Control type="email" placeholder="Correo electrónico" required />
+      </div>
+      <div className="form-group mb-4">
+        <Form.Control type="password" placeholder="Contraseña" required />
+      </div>
+      <div className="form-group mb-4">
+        <Form.Control type="password" placeholder="Repite la contraseña" required />
+      </div>
+      <div className="form-group mb-4">
+        <Form.Control type="tel" placeholder="Número de teléfono" required />
+      </div>
+      <div className="form-group mt-3">
+        <Button variant="primary" type="submit" className="form-control btn btn-primary submit px-3">Registrarse</Button>
+      </div>
+    </Form>
+  );
+}
 
 function FormularioLogin() {
+  const [mostrarRegistro, setMostrarRegistro] = useState(false);
+
+  const toggleMostrarRegistro = () => {
+    setMostrarRegistro(!mostrarRegistro);
+  };
+  const textoEnlace = mostrarRegistro ? 'Volver a iniciar sesión' : '! Soy nuevo ! | Registrarse';
+
   return (
-    <div className="container mb-0 mt-5 ">
-      
-        <div className="col-12 text-center mb-4 mt-5">
+    <div className="container">
+      <div className="col-12 text-center mb-4">
         <Link to="/">
           <img
             src={Logo}
@@ -20,35 +51,46 @@ function FormularioLogin() {
             alt="Logo"
           />
         </Link>
-        </div>
-      
+      </div>
       <div className="row justify-content-center">
         <div className="col-md-10 col-lg-6 col-sm-10">
           <div className="login p-0">
-            <Form className="signin-form">
-              <div className="form-group mb-4">
-                <Form.Control type="text" placeholder="Usuario" required />
-              </div>
-              <div className="form-group mb-4">
-                <Form.Control type="password" placeholder="Contraseña" required />
-                <span className="fa fa-fw fa-eye field-icon toggle-password" />
-                <div className="text-md-end mt-2">
-                  <a href="#">Olvidé la contraseña</a>
+            {mostrarRegistro ? (
+              <RegistroForm />
+            ) : (
+              <Form className="signin-form">
+                {/* Agrega aquí los campos del formulario de inicio de sesión */}
+                <div className="form-group mb-4">
+                  <Form.Control type="text" placeholder="Usuario" required />
                 </div>
-              </div>
-              <div className="form-group mt-3">
-                <Button variant="primary" type="submit" className="form-control btn btn-primary submit px-3">Iniciar Sesión</Button>
-              </div>
-              <div className="form-group d-md-flex justify-content-center mt-3">
-                <div className="w-100 d-flex justify-content-center">
-                  <a href="#">! Soy nuevo ! | registrarse</a>
+                <div className="form-group mb-4">
+                  <Form.Control type="password" placeholder="Contraseña" required />
+                  <span className="fa fa-fw fa-eye field-icon toggle-password" />
+                  <div className="text-md-end mt-2">
+                    <a href="#">Olvidé la contraseña</a>
+                  </div>
                 </div>
+                <div className="form-group mt-3">
+                  <Button variant="primary" type="submit" className="form-control btn btn-primary submit px-3">Iniciar Sesión</Button>
+                </div>
+                <p className="w-100 text-center mt-3">— O inicia sesión con —</p>
+                <div className="social d-flex text-center justify-content-center">
+                <a href="#" className="px-2 py-2 fs-3">
+                  <FaGoogle className="icon mb-1" /> Google
+                </a>
+                <a href="#" className="px-2 py-2 fs-3">
+                  <FaApple className="icon mb-1 " /> Apple
+                </a>
+                </div>
+              </Form>
+              
+            )}
+            <div className="form-group d-md-flex justify-content-center mt-3">
+              <div className="w-100 d-flex justify-content-center mt-1">
+                <p>
+                <Link to="#" onClick={toggleMostrarRegistro}>{textoEnlace}</Link>
+                </p>
               </div>
-            </Form>
-            <p className="w-100 text-center mt-4">— O inicia sesión con —</p>
-            <div className="social d-flex text-center justify-content-center">
-              <a href="#" className="px-2 py-2 mr-md-1 rounded"><i className="bi bi-google me-1" /> Google</a>
-              <a href="#" className="px-2 py-2 ml-md-1 rounded"><i className="bi bi-apple me-1" /> Apple</a>
             </div>
           </div>
         </div>
